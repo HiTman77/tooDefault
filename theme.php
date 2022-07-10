299adddb48180454cdc6ccb66ed8ea4f93d312c1
HiTman77 <doug@kadabra.co.za>
2022-07-10 11:31:37 +0200

    v

--- theme.php
+++ theme.php
@@ -44,7 +44,7 @@
 <div class="row customBackground">
 <div class="col-lg-12 my-auto text-left padding40 resetTextRotation"><?= $Wcms->block('subside') ?></div></div></section>
 <footer class="mt-4 footer">
-<div class="container-fluid py-3 text-right"><?= $Wcms->footer() ?></div></footer>
+<div class="container-fluid py-3 text-left"><?= $Wcms->footer() ?></div></footer>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous" defer></script>
